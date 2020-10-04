7b0925ef79b2b6ff7ff3d4a07c3de82ae860d73f
Roman Avdonin <gordonbuble49@gmail.com>
2020-10-05 00:37:00 +0300

    Initial Commit

---
+++ techNewsApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  techNewsApp
+//
+//  Created by Яблочко on 05.10.2020.
+//  Copyright © 2020 Roman Avdonin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
